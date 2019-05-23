e53eaec19dba415a4003e930c87748fb33ce77ed
lcazev <40309895+lcazev@users.noreply.github.com>
2019-05-23 14:41:11 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -1,4 +1,26 @@
-
 public class Livro {
+    private String titulo;
+    private String autor;
+
+    public Livro(String autor, String titulo) {
+        this.setAutor(autor);
+        this.setTitulo(titulo);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 
 }
