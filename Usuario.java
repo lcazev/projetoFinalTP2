cc5e6ed6f0a2cc8f5b72ea601b94a3146694dbad
lcazev <40309895+lcazev@users.noreply.github.com>
2019-05-23 15:09:08 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,4 +1,46 @@
 
 public class Usuario {
+    private String nome;
+    private String email;
+    private Endereco endereco;
+    
+    public Usuario(String nome, Endereco endereco, String email){
+        this.setNome(nome);
+        this.setEndereco(endereco);
+        this.setEmail(email);
+        
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+       if(emailValido(email)){
+           this.email = email;
+       } else {
+           System.out.println("TEMOS QUE COLOCAR UMA EXCEÇÃO AQUI MAS NAO SEI QUAL RSRSRS");
+       }
+    }
+
+    public String getEndereco(String endereco) {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    public boolean emailValido(String email){
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);    }
+    
 }
