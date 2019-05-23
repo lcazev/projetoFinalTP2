ffaaf47f54626ca2ca4efa8503580795ef4e5368
lcazev <40309895+lcazev@users.noreply.github.com>
2019-05-23 15:33:53 -0300

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
-public class Biblioteca {
+public class Biblioteca extends Usuario {
+    private ArrayList<String> livrosRecebidos;
+    private ArrayList<String> catalogo;
+
+    public Biblioteca(String nome, Endereco endereco, String email, ArrayList<String> catalogo /*criar arq texto com catalogos*/) {
+        super(nome, endereco, email);
+        this.setCatalogo(catalogo);
+        @SuppressWarnings("unused")
+        ArrayList<String> livrosRecebidos;
+    }
+
+    public ArrayList<String> getLivrosRecebidos() {
+        return livrosRecebidos;
+    }
+
+    public ArrayList<String> getCatalogo() {
+        return catalogo;
+    }
+
+    public void setCatalogo(ArrayList<String> catalogo) {
+        this.catalogo = catalogo;
+    }
 
 }
