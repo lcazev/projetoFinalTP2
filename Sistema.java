3a906bebe469804dbc2834f41e870df2fffd7015
lcazev <40309895+lcazev@users.noreply.github.com>
2019-05-23 16:38:08 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -2,11 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sistema {
+    static Scanner entrada = new Scanner(System.in);
     
     static ArrayList<Biblioteca> bibliotecas = new ArrayList<Biblioteca>();//aqui temos que ter um arquivo de texto com todas as bibs
 
-    public void pedirLivro(Livro livro) {
-        FilaEncaminhados.encaminharLivro(livro);
+    public static void pedirLivro() {
+        System.out.println("Bem-vindo(a)! Digite o titulo do livro que deseja pedir:");
+        String titulo = entrada.nextLine();
+        //chamar a funcao obterLivroPorTitulo
+        FilaEncaminhados.encaminharLivro(null);
     }
     
     public boolean obterLivroPorTitulo(String livro){
@@ -16,13 +20,43 @@ public class Sistema {
         return true;
     }
     
+    public static void cadastrarUsuario(){
+        
+    }
+    
+    public static void cadastrarBiblioteca(){
+        
+    }
+    
+    public static void avaliarEntrega(){
+        
+    }
+
     //montando uma interface simples para testar o funcionamento
     public static void main(String[] args) 
     { 
-        Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Bem-vindo(a)! Digite o titulo do livro que deseja pedir:");
-        String titulo = entrada.nextLine();
+        System.out.println("Você é:");
+        System.out.println("1) Usuario 2)Biblioteca");
+        int opcao = entrada.nextInt();
+        switch(opcao){
+        case 1:
+            System.out.println("1) Cadastrar usuario ou 2) Pedir livro");
+            opcao = entrada.nextInt(); //tratar caso de numero errado ou letra
+            switch (opcao){
+            case 1: cadastrarUsuario();
+            case 2: pedirLivro(); 
+            break;
+            }
+        case 2: 
+            System.out.println("1) Cadastrar biblioteca ou 2) Avaliar entrega");
+            opcao = entrada.nextInt();
+            switch(opcao){
+            case 1: cadastrarBiblioteca();
+            case 2: avaliarEntrega();
+            }
+        
+        }
         
     }
 }
