3d05b56f5d27099d5461dc2c6e08350fb8f71738
Luiza Agostinho <39842711+luizaagostinho@users.noreply.github.com>
2019-06-09 11:16:37 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,19 +1,49 @@
-package classes;
+package classes.modelo.atividades;
 
 import java.util.ArrayList;
 
 public class Pedido extends Atividade { 
-   // Pedido precisa herdar também Usuário? 
+   // Pedido precisa herdar tambï¿½m Usuï¿½rio? 
    //Pedido deve ter os dados de quem o fez.
 	
 	ArrayList<String> pedidos;
 	
 	public Pedido() {
-		// os dados do usuário precisam ser passados ao fazer um pedido?
+		// os dados do usuï¿½rio precisam ser passados ao fazer um pedido?
 		// o que efetivamente precisa ser informado ao se fazer um pedido?
 	}
 	
 	public void pedeLivro(String titulo) {
 		this.pedidos.add(titulo);
+	}
+
+	@Override
+	public boolean buscaLivro() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void doaLivro() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean entregaLivro() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void pedeLivro() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recebeLivro() {
+		// TODO Auto-generated method stub
+		
 	}
 }
