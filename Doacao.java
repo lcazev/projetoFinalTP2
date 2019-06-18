0b897f9a3ff3f4f02938f5d56d1363af0786281f
lcazev <40309895+lcazev@users.noreply.github.com>
2019-06-18 00:49:33 -0300

    Update Doacao.java

--- Doacao.java
+++ Doacao.java
@@ -2,43 +2,63 @@ package classes.modelo.atividades;
 
 import java.util.ArrayList;
 
-public class Doacao extends Atividade{
-	private String titulo;
-	
-	public void doaLivro(ArrayList<String> catalogo, String titulo) {
-		if(entregaLivro()) {
-			catalogo.add(titulo);	
+public class Doacao extends Atividade {
+	public static ArrayList<Doacao> doacoes;
+	private Livro livroDoado;
+	private Usuario doador;
+
+	public Doacao(Usuario usuario, Livro livro) {
+		this.setLivroDoado(livro);
+		ArrayList<Doacao> doacoes = new ArrayList<Doacao>();
+	}
+
+	public Usuario getDoador() {
+		return doador;
+	}
+
+	public void setDoador(Usuario doador) {
+		this.doador = doador;
+	}
+
+	public Livro getLivro() {
+		return livroDoado;
+	}
+
+	public void setLivroDoado(Livro livro) {
+		this.livroDoado = livro;
+	}
+
+	public void definirBiblioteca(Livro livro) {
+		for (int i = 0; i < Pedido.pedidos.size(); i++) {
+			for (int j = 0; j < Biblioteca.bibliotecasSistema.size(); j++) {
+				if (!Biblioteca.bibliotecasSistema.get(j).getCatalogo().contains(livro)) {
+					if (Pedido.pedidos.get(i).getAutorPedido().getEndereco()
+							.equals(Biblioteca.bibliotecasSistema.get(j).getEndereco())) {
+						doarLivro(livro, Biblioteca.bibliotecasSistema.get(j),
+								Pedido.pedidos.get(i).getAutorPedido().getEndereco());
+					}
+
+				}
+
+			}
 		}
+
+	}
+
+	public void doarLivro(Livro livro, Biblioteca biblioteca, String endereco) {
+		// relacionar este método com o método realizarEntrega
+
+	}
+
+	public void gerarDoacao() {
+		doacoes.add(this);
+		registrarAtividade("Doacao de livro" + this.getLivro() + " pelo usuario " + this.getDoador());
 	}
 
 	@Override
-	public boolean buscaLivro() {
-		// TODO Auto-generated method stub
-		return false;
+	public void registrarAtividade(String atividade) {
+		Atividade.registroAtividade.add(atividade);
+
 	}
 
-	@Override
-	public void doaLivro() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean entregaLivro() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void pedeLivro() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void recebeLivro() {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
