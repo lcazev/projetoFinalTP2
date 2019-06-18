88513a5f5baec75221e38c9537e97ed40c194e71
educoelho96 <39635958+educoelho96@users.noreply.github.com>
2019-06-17 23:27:20 -0300

    Update LivroInt.java

--- LivroInt.java
+++ LivroInt.java
@@ -11,24 +11,5 @@ public class LivroInt extends Atividade {
 		System.out.println("Bem-vindo(a)! Digite o titulo do livro que deseja pedir:");
 		Sistema.entrada.nextLine();
 		String titulo = Sistema.entrada.nextLine();
-		
-		obterLivroPorTitulo(titulo);
-	}
-
-	private static void obterLivroPorTitulo(String livro) {
-		
-		List<Biblioteca> encontradas = Sistema.bibliotecas.getBibliotecasQuePossuemOLivro(livro);
-		
-		boolean livroNaoEncontrado = encontradas == null;
-		
-		if(livroNaoEncontrado) {
-			System.out.println("Livro nao encontrado. Redirecionando para pedidos");
-			
-		} else {
-			for(Biblioteca biblioteca : encontradas) {
-				System.out.println("Este livro está disponivel na biblioteca "+biblioteca.getNome()+
-								   " no endereco "+biblioteca.getEndereco());
-			}
-		}
 	}
 }
