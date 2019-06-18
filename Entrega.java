3e35e75b26a1d225827fc2b2a8f73d3f918802a5
lcazev <40309895+lcazev@users.noreply.github.com>
2019-06-18 00:51:58 -0300

    Update Entrega.java

--- Entrega.java
+++ Entrega.java
@@ -1,9 +1,24 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class Entrega {
 
-	
-	public boolean entregaLivro() {
-		return true;
+	private ArrayList<Usuario> voluntariosEntrega;
+	private Usuario entregador;
+
+	public Usuario setEntregador(Biblioteca biblioteca) {
+		for (int i = 0; i < voluntariosEntrega.size(); i++) {
+			if (voluntariosEntrega.get(i).getEndereco().equals(biblioteca.getEndereco())) {
+				return voluntariosEntrega.get(i);
+			}
+		}
+		return null;
+	}
+
+	public void realizarEntrega(Livro livro, Biblioteca biblioteca, Endereco endereco) {
+		// enviar mensagem ao entregador definido em setEntregador
+		// para que busque o livro no endereço mostrado
+		// e leve-o na biblioteca mostrada
 	}
 }
