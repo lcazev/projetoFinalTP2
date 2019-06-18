4a45d22b064b10fac9db37fde65d695bc20429f6
Luiza Agostinho <39842711+luizaagostinho@users.noreply.github.com>
2019-06-18 00:59:54 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,15 +1,15 @@
-package classes.modelo.atividades;
+package classes;
 
 import java.util.ArrayList;
 
-public class Pedido extends Atividade { 
-   // Pedido precisa herdar tamb�m Usu�rio? 
+public abstract class Pedido extends Atividade { 
+   // Pedido precisa herdar também Usuário? 
    //Pedido deve ter os dados de quem o fez.
 	
 	ArrayList<String> pedidos;
 	
 	public Pedido() {
-		// os dados do usu�rio precisam ser passados ao fazer um pedido?
+		// os dados do usuário precisam ser passados ao fazer um pedido?
 		// o que efetivamente precisa ser informado ao se fazer um pedido?
 	}
 	
