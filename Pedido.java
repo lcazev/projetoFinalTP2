93335e35774c9c5356fd4191dbf711b54c1b3d00
lcazev <40309895+lcazev@users.noreply.github.com>
2019-06-25 16:54:02 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,49 +1,36 @@
-package classes;
+package bookSmart;
 
 import java.util.ArrayList;
 
 public abstract class Pedido extends Atividade { 
-   // Pedido precisa herdar também Usuário? 
-   //Pedido deve ter os dados de quem o fez.
+ private Biblioteca bibOrigem;
+ private Livro livroPedido; 
+static ArrayList<Pedido> pedidos;
 	
-	ArrayList<String> pedidos;
+	public Pedido(Biblioteca bib, Livro livro) {
+	    this.setBibOrigem(bib);
+	    this.setLivroPedido(livro);
+	    pedidos.add(this);
 	
-	public Pedido() {
-		// os dados do usuário precisam ser passados ao fazer um pedido?
-		// o que efetivamente precisa ser informado ao se fazer um pedido?
 	}
+
+    public Biblioteca getBibOrigem() {
+        return bibOrigem;
+    }
+
+    public void setBibOrigem(Biblioteca bibOrigem) {
+        this.bibOrigem = bibOrigem;
+    }
+
+    public Livro getLivroPedido() {
+        return livroPedido;
+    }
+
+    public void setLivroPedido(Livro livroPedido) {
+        this.livroPedido = livroPedido;
+    }
+    
 	
-	public void pedeLivro(String titulo) {
-		this.pedidos.add(titulo);
-	}
-
-	@Override
-	public boolean buscaLivro() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void doaLivro() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean entregaLivro() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void pedeLivro() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void recebeLivro() {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	
 }
