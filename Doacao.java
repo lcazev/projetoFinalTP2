a42f7e341af33cb145d9d0a6d0f6b7ea91234f02
lcazev <40309895+lcazev@users.noreply.github.com>
2019-06-27 15:05:33 -0300

    Update Doacao.java

--- Doacao.java
+++ Doacao.java
@@ -9,7 +9,9 @@ public class Doacao extends Atividade {
 
 	public Doacao(Usuario usuario, Livro livro) {
 		this.setLivroDoado(livro);
-		ArrayList<Doacao> doacoes = new ArrayList<Doacao>();
+		this.setDoador(usuario);
+		doacoes.add(this); //adiciona esta doacao a lista geral de doacoes
+		registrarAtividade(gerarDoacao());//adiciona esta doacao a lista geral de atividades
 	}
 
 	public Usuario getDoador() {
@@ -28,35 +30,12 @@ public class Doacao extends Atividade {
 		this.livroDoado = livro;
 	}
 
-	public void definirBiblioteca(Livro livro) {
-		for (int i = 0; i < Pedido.pedidos.size(); i++) {
-			for (int j = 0; j < Biblioteca.bibliotecasSistema.size(); j++) {
-				if (!Biblioteca.bibliotecasSistema.get(j).getCatalogo().contains(livro)) {
-					if (Pedido.pedidos.get(i).getAutorPedido().getEndereco()
-							.equals(Biblioteca.bibliotecasSistema.get(j).getEndereco())) {
-						doarLivro(livro, Biblioteca.bibliotecasSistema.get(j),
-								Pedido.pedidos.get(i).getAutorPedido().getEndereco());
-					}
-
-				}
-			}
-		}
-
+	public String getBairroDoacao() {
+		return this.getDoador().getBairro();
 	}
 
-	public void doarLivro(Livro livro, Biblioteca biblioteca, String endereco) {
-		// relacionar este método com o método realizarEntrega
-		// precisa-se saber o endereço da biblioteca que fez o pedido
-		// e do usuário que fará a doação para que o usuário entregador
-		// possa entregar a doação.
-		// o processo de doação é pré-iniciado quando há o "match" entre biblioteca, doador e entregador
-		// e começa de fato quando o entregador recolhe a doação no endereço do doador.
-		// Uma doação é concluída quando a entrega é finalizada.
-	}
-
-	public void gerarDoacao() {
-		doacoes.add(this);
-		registrarAtividade("Doacao de livro" + this.getLivro() + " pelo usuario " + this.getDoador());
+	public String gerarDoacao() {
+		return "Doacao de livro" + this.getLivro() + " pelo usuario " + this.getDoador();
 	}
 
 	@Override
@@ -64,5 +43,4 @@ public class Doacao extends Atividade {
 		Atividade.registroAtividade.add(atividade);
 
 	}
-
 }
