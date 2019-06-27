8bcc4f4b0b8fd36684af24ababb5d24061798e54
lcazev <40309895+lcazev@users.noreply.github.com>
2019-06-27 14:48:23 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -3,34 +3,41 @@ package bookSmart;
 import java.util.ArrayList;
 
 public abstract class Pedido extends Atividade { 
- private Biblioteca bibOrigem;
- private Livro livroPedido; 
-static ArrayList<Pedido> pedidos;
-	
+	private Biblioteca bibOrigem;
+	private Livro livroPedido;
+	static ArrayList<Pedido> pedidos;
+
 	public Pedido(Biblioteca bib, Livro livro) {
-	    this.setBibOrigem(bib);
-	    this.setLivroPedido(livro);
-	    pedidos.add(this);
-	
+		this.setBibOrigem(bib);
+		this.setLivroPedido(livro);
+		pedidos.add(this); // o pedido vai para a fila no ato de sua criacao
+		registrarAtividade(gerarPedido()); // o registro do pedido vai para o log de pedidos
+
 	}
 
-    public Biblioteca getBibOrigem() {
-        return bibOrigem;
-    }
+	public Biblioteca getBibOrigem() {
+		return bibOrigem;
+	}
 
-    public void setBibOrigem(Biblioteca bibOrigem) {
-        this.bibOrigem = bibOrigem;
-    }
+	public void setBibOrigem(Biblioteca bibOrigem) {
+		this.bibOrigem = bibOrigem;
+	}
 
-    public Livro getLivroPedido() {
-        return livroPedido;
-    }
+	public Livro getLivroPedido() {
+		return livroPedido;
+	}
 
-    public void setLivroPedido(Livro livroPedido) {
-        this.livroPedido = livroPedido;
-    }
-    
-	
-	
-	
+	public void setLivroPedido(Livro livroPedido) {
+		this.livroPedido = livroPedido;
+	}
+
+	public String gerarPedido() {
+		return "Livro" + this.getLivroPedido().getTitulo() + " solicitado por " + this.getBibOrigem().getNome();
+	}
+
+	@Override
+	public void registrarAtividade(String atividade) {
+		registroAtividade.add(atividade);
+
+	}
 }
