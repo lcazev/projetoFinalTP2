36e1f3e16d4ffa09d596ffb3acc126512b86dc02
lcazev <40309895+lcazev@users.noreply.github.com>
2019-06-28 20:51:28 -0300

    Update Entregador.java

--- Entregador.java
+++ Entregador.java
@@ -3,16 +3,23 @@ package bookSmart;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Entregador extends Usuario {
+public class Entregador extends Usuario{
 
 	static Scanner entrada = new Scanner(System.in);
-	private ArrayList<Bairro> bairrosAtendidos;
+	protected static ArrayList<Entregador> voluntariosEntrega=new ArrayList<Entregador>();
+	//instanciar alguns voluntarios aqui.
+	private Usuario entregador;
+	protected ArrayList<Bairro> bairrosAtendidos;
+	protected ArrayList<Pedido> entregasPendentes;
 
-	public Entregador(String nome, Endereco endereco, String email, String senha) {
+	public Entregador(String nome, Endereco endereco, String email, String senha){
 		super(nome, endereco, email, senha);
 		ArrayList<Bairro> bairrosAtendidos = new ArrayList<Bairro>();
+		ArrayList<Pedido> entregasPendentes = new ArrayList<Pedido>();
+		escolherBairros();//abre a lista de bairros pro novo entregador escolher onde quer atuar
 	}
-
+	
+	
 	public void escolherBairros() {
 		exibirBairros();
 		System.out.println("Digite os numeros dos bairros que deseja atender. Para parar, digite -1");
@@ -24,10 +31,16 @@ public class Entregador extends Usuario {
 				bairrosAtend.add(num);
 			}
 		} while (num > 0);
+		//essa funcao retorna uma lista com os indices escolhidos
 		adicionarBairrosLista(bairrosAtend);
+		//aqui temos que inserir a funcao pra retornar ao inicio do ambiente de entregador.
+		//talvez seja melhor trazer os ambientes para as classes, mas nao sei se e possivel.
 	}
 
 	public void adicionarBairrosLista(ArrayList<Integer> lista) {
+		//essa funcao pega a lista com os indices gerada acima
+		//e adiciona na lista de bairros do entregador que escolheu
+		//eu nao sei se ta rodando direito essa parte, temos que analisar
 		for (int indice : lista) {
 			for (Bairro bairro : Bairro.values()) {
 				if (indice == bairro.getNumBairro()) {
@@ -35,6 +48,7 @@ public class Entregador extends Usuario {
 				}
 			}
 		}
+		voluntariosEntrega.add(this);
 	}
 
 	public void exibirBairros() {
@@ -46,3 +60,11 @@ public class Entregador extends Usuario {
 			System.out.println(nome.getNumBairro() + " " + nome.getBairro());
 		}
 	}
+	
+	public void exibirEntregasPendentes() {
+		//eu acho que inutilizei essa funcao já mostrando a lista de entregas
+		//pendentes no ambiente do entregador
+		//mas como nao sei se essa e a saida mais legal, vou deixar ela aqui
+		//pro caso de movermos os ambientes para dentro das classes. 
+	}
+}
