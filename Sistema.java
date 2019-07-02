ff2ea7cdcca127a764b68c10af7da670fe28bb0c
educoelho96 <39635958+educoelho96@users.noreply.github.com>
2019-07-01 23:00:17 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -27,11 +27,11 @@ public class Sistema {
 	static ArrayList<Biblioteca> bibliotecas = new ArrayList<Biblioteca>();// arquivo com todas as bibs
 
 	public static void menu() {
-		System.out.println("Bem vindo ao BookSmart! Selecione uma opcao:");
-		System.out.println("1) Cadastrar usuario");
+		System.out.println("Bem-vindo ao BookSmart! Selecione uma opção:");
+		System.out.println("1) Cadastrar usuário");
 		System.out.println("2) Cadastrar biblioteca");
-		System.out.println("3) Entrar como usuario");// acho que aqui direciona pro ambiente de user comum, temos que
-														// diferenciar do entregador
+		System.out.println("3) Entrar como usuário");// acho que aqui direciona pro ambiente de user comum, temos que
+							     // diferenciar do entregador
 		System.out.println("4) Entrar como biblioteca");
 		int opcaoMenu = entrada.nextInt();
 		switch (opcaoMenu) {
@@ -44,13 +44,13 @@ public class Sistema {
 	}
 
 	public static void cadastrarUsuario() {
-		System.out.println("Digite o nome: ");
+		System.out.println("Digite o seu nome: ");
 		String nome = entrada.nextLine();
 		System.out.println("Digite seu endereco:");
 		System.out.println("Logadouro: ");
 		String logadouro = entrada.nextLine();
 		entrada.nextLine();
-		System.out.println("Numero: ");
+		System.out.println("Número: ");
 		int numero = entrada.nextInt();
 		System.out.println("Complemento: ");
 		String complemento = entrada.nextLine();
@@ -69,11 +69,11 @@ public class Sistema {
 		case 1:
 			Usuario.usuarios
 					.add(new Usuario(nome, new Endereco(logadouro, numero, complemento, bairro, cidade), email, senha));
-			System.out.println(" Usuario cadastrado com sucesso! Fazer login: ");
+			System.out.println(" Usuário cadastrado com sucesso! Fazer login: ");
 			entrar();
 		case 2:
 			ArrayList<Livro> cadastroCatalogo = new ArrayList<Livro>();
-			System.out.println("Adicione os titulos ao seu catalogo. Para parar, digite 0: ");
+			System.out.println("Adicione os titulos ao seu catálogo. Para parar, digite 0: ");
 			while (true) {
 				System.out.println("Adicione titulo:");
 				String titulo = entrada.nextLine();
@@ -106,7 +106,7 @@ public class Sistema {
 					} else if (biblioteca.getEmail().equals(email) && biblioteca.getSenha().equals(senha)) {
 						ambienteUser(biblioteca);
 					} else {
-						System.out.println("Login ou senha invalidos! Tente novamente: ");
+						System.out.println("Login ou senha inválidos! Tente novamente: ");
 						entrar();
 					}
 				}
@@ -116,7 +116,7 @@ public class Sistema {
 
 	public static void ambienteUser(Usuario usuario) {
 
-		System.out.println("Ola! O que deseja fazer?");
+		System.out.println("Olá! O que deseja fazer?");
 		System.out.println("1) Doar livro");
 		System.out.println("2) Tornar-se um entregador");
 		int opcaoComum = entrada.nextInt();
@@ -130,25 +130,25 @@ public class Sistema {
 	}
 
 	public static void ambienteUser(Entregador entregador) {
-		System.out.println("Ola! O que deseja fazer?");
+		System.out.println("Olá! O que deseja fazer?");
 		System.out.println("1) Doar livro");
 		System.out.println("2) Realizar entrega");
 		int opcaoEntregador = entrada.nextInt();
 		switch (opcaoEntregador) {
 		case 1:
 		case 2:
-			System.out.println("Aqui estao suas entregas pendentes:");
+			System.out.println("Aqui estão suas entregas pendentes:");
 			System.out.println(entregador.entregasPendentes.toString());
 		}
 	}
 	
 	public static void ambienteUser(Biblioteca biblioteca) {
-		System.out.println("Ola! O que deseja fazer?");
+		System.out.println("Olá! O que deseja fazer?");
 		System.out.println("1) Pedir livro");
 		System.out.println("2) Confirmar o recebimento de um livro");
 		int opcaoBiblioteca = entrada.nextInt();
 		switch(opcaoBiblioteca) {
-		case 1: System.out.println("Digite o titulo do livro: ");
+		case 1: System.out.println("Digite o título do livro: ");
 		String titulo = entrada.nextLine();
 		System.out.println("Digite o autor do livro: ");
 		String autor = entrada.nextLine();
@@ -160,7 +160,7 @@ public class Sistema {
 
 	public static void main(String[] args) {
 	new Usuario("Laryssa Azevedo", new Endereco("Rua General Roca", 350, "apto 301", "Tijuca", "Rio de Janeiro"), "laryssa.azevedo@uniriotec.br", "123456");
-	new Usuario("Eduardo", new Endereco("Rua Voluntarios da Patria", 12, "s/n", "Botafogo", "Rio de Janeiro"), "eduardo.coelho@uniriotec.br", "123456");
+	new Usuario("Eduardo Coelho", new Endereco("Rua Voluntarios da Patria", 12, "s/n", "Botafogo", "Rio de Janeiro"), "eduardo.coelho@uniriotec.br", "123456");
 	new Usuario("Luiza Agostinho", new Endereco("Avenida Pasteur", 100, "apto 1010", "Urca", "Rio de Janeiro"), "luiza.agostinho@uniriotec.br", "123456");
 	new Usuario("Maria Silva", new Endereco("Rua Marques de Sao Vicente", 925, "apto 904", "Gavea", "Rio de Janeiro"), "maria.silva@uniriotec.br", "123456");
 	new Usuario("Julio Sousa", new Endereco("Rua Quito", 10, "casa", "Penha", "Rio de Janeiro"), "julio.sousa@uniriotec.br", "123456");
